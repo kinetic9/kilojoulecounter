267c78480957f878f586d98ff9decc616a68b0d3
Bongani Khoza <bonganebnk@gmail.com>
2019-08-11 15:24:57 +0200

    activity for diary entry added

---
+++ app/src/main/java/com/example/kilojoulecounter/DiaryEntry.java
@@ -0,0 +1,14 @@
+package com.example.kilojoulecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiaryEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_entry);
+    }
+}
