0c6fe317aacd292ce1a54aa2ac0bf3e2f88b774a
Bongani Khoza <bonganebnk@gmail.com>
2019-08-11 14:48:59 +0200

    Basic clicking works

--- app/src/main/java/com/example/kilojoulecounter/DiaryAdapter.java
+++ app/src/main/java/com/example/kilojoulecounter/DiaryAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,9 +34,17 @@ public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.MyViewHolder
     }
 
     public void onBindViewHolder(MyViewHolder holder, int position){
-        Diary diary = diaryList.get(position);
+        final Diary diary = diaryList.get(position);
         holder.date.setText(diary.getDate());
         holder.nki.setText(Double.toString(diary.nettKiloJoule()));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Clicked Item"+diary.getBreakfast(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     public int getItemCount(){
---
+++ app/src/main/java/com/example/kilojoulecounter/RecyclerTouchListener.java
@@ -0,0 +1,13 @@
+package com.example.kilojoulecounter;
+
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerTouchListener implements RecyclerView.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
